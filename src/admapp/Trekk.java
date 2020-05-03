91043de7d1a091d5933450a28b1dfc235f03b9d9
Henriksj65 <46962172+Henriksj65@users.noreply.github.com>
2020-05-03 13:40:45 +0200

    commit

---
+++ src/admapp/Trekk.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admapp;
+
+/**
+ *
+ * @author henriksj
+ */
+public class Trekk{
+    
+    private String kommentar;
+    private String tekst_kode;
+    
+   
+
+    public String getKommentar() {
+        return kommentar;
+    }
+
+    public void setKommentar(String kommentar) {
+        this.kommentar = kommentar;
+    }
+
+    public String getTekst_kode() {
+        return tekst_kode;
+    }
+
+    public void setTekst_kode(String tekst_kode) {
+        this.tekst_kode = tekst_kode;
+    }
+    
+    
+}
